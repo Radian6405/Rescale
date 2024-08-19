aab464ac4da2057df3ae6b470d52eff874da4be3
Sai Vardhan <vardhan.6405@gmail.com>
2024-08-19 23:25:31 +0530

    fix: mass label for pickups now show properly for any scale of objects

--- scenes/pickups/pickup.gd
+++ scenes/pickups/pickup.gd
@@ -5,7 +5,9 @@ class_name pickup_object
 @onready var scale_controller: Node3D = $ScaleController
 @onready var collidor: CollisionShape3D = $Collidor
 @onready var pickup_collidor: CollisionShape3D = $pickuphelper/PickupCollidor
-@onready var mass_label: Label3D = $ScaleController/MassLabel
+@onready var mass_label_controller: Node3D = $MassLabelController
+@onready var mass_label: Label3D = $MassLabelController/MassLabel
+@onready var init_mass_label_local_pos: Vector3 = mass_label.position
 
 @export var initial_scale := 1.0
 @export var max_scale := 1.5
@@ -14,8 +16,10 @@ class_name pickup_object
 var current_scale: float = initial_scale:
 	set(value):
 		current_scale = clamp(value, min_scale, max_scale)
-		scale_controller.scale = Vector3.ONE * current_scale
+		
 		collidor.scale = Vector3.ONE * current_scale
+		scale_controller.scale = Vector3.ONE * current_scale
+		mass_label_controller.scale = Vector3.ONE * current_scale
 		
 		if current_scale >= 1:
 			pickup_collidor.scale = Vector3.ONE * current_scale
@@ -25,6 +29,14 @@ var current_scale: float = initial_scale:
 func _ready() -> void:
 	current_scale = initial_scale
 	mass_label.text = "Mass: " + str(mass)
+
+func _physics_process(delta: float) -> void:
+	# position adjustment for mass label at any mass
+	mass_label.global_position = (
+		mass_label_controller.global_position + 
+		init_mass_label_local_pos + 
+		Vector3(0,current_scale - 0.9,0) # idk why this works but it does
+		)
 
 func handle_grab()-> void:
 	print("pick")
