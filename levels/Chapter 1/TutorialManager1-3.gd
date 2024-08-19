b3cc95df9e275868f857edf8d318260f56c9ef1b
Sai Vardhan <vardhan.6405@gmail.com>
2024-08-19 18:58:40 +0530

    feat: additional tutorial label for level 1-3

---
+++ levels/Chapter 1/TutorialManager1-3.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var label_1: Label = $VBoxContainer2/Label1
+
+func _ready() -> void:
+	label_1.visible = false
+
+func _on_tut_1_area_body_entered(body: Node3D) -> void:
+	if body is player:
+		label_1.visible = true
+
+
+func _on_tut_1_area_body_exited(body: Node3D) -> void:
+	if body is player:
+		label_1.visible = false
